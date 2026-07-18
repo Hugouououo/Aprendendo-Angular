f060b706067c11c558f670394c88f037dd7c84f2
Hugo <hugomoreiralaprano@gmail.com>
2026-07-18 15:17:12 -0300

    preparando pra parte chata (backend)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -15,7 +15,7 @@ import { QuadroCadastroComponent } from './components/quadro-cadastro/quadro-cad
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
-})\
+})
 export class AppComponent {
   title = 'teste';
 }
---
+++ frontend/src/app/services/usuario/usuario.model.ts
@@ -0,0 +1,5 @@
+export interface Usuario {
+    id?: string //json server gera
+    usuario: string
+    senha: string
+}
